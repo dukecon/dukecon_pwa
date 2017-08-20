5a3e2fa11adaca2052a4324460f6f523654ce157
Alexander Schwartz <alexander.schwartz@gmx.net>
2017-08-20 16:48:57 +0200

    initial commit after vue init

---
+++ src/components/ScheduledEvent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="talk">
+    <h1>Talk</h1>
+    <p>{{ $route.params.talkId }}</p>
+    <p>Abstract {{fullMessage}}</p>
+    <p>Speaker</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'talk',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js PWA' + this.$route.params.talkId
+    }
+  },
+  computed: {
+    fullMessage: function () {
+      return 'Welcome to Your Vue.js PWA' + this.$route.params.talkId
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
