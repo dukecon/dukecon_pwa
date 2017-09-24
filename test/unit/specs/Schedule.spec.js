dcc19598bc21a73a2668f2ca342854daa8a176f3
Alexander Schwartz <alexander.schwartz@gmx.net>
2017-09-24 16:24:11 +0200

    avoid warnings to missing router

--- test/unit/specs/Schedule.spec.js
+++ test/unit/specs/Schedule.spec.js
@@ -1,11 +1,32 @@
 import Vue from 'vue'
 import Schedule from '@/components/Schedule'
+import Router from 'vue-router'
 
 describe('Schedule.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(Schedule)
-    const vm = new Constructor().$mount()
+    // given ...
+    // ... and a router
+    Vue.use(Router)
+    const router = new Router({
+      routes: [
+        {
+          path: '/scheduledEvent/:eventId',
+          name: 'scheduledEvent'
+        }
+      ]
+    })
+    // ... and a Vue instance with the component
+    const vm = new Vue({
+      components: {
+        Schedule
+      },
+      template: '<div><schedule></schedule></div>',
+      router
+    }).$mount()
+    // then ...
+    // ... item is shown
     expect(vm.$el.querySelector('.schedule h1').textContent)
       .to.equal('Schedule')
+    // TODO: mock conference to see that events are properly shown
   })
 })
