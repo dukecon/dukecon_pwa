0a80239acd2b53e92ce7d5039b9e99b874ca5dcf
Alexander Schwartz <alexander.schwartz@gmx.net>
2017-10-29 18:57:02 +0100

    update mock to prevent exceptions in component #24

--- test/unit/specs/Search.spec.js
+++ test/unit/specs/Search.spec.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Search from '@/components/navbar/Search'
 import { i18n } from '@/Internationalization.js'
 import Eventbus from '../../../src/Eventbus'
+import Router from 'vue-router'
 
 describe('Search.vue', () => {
   var sandbox
@@ -19,12 +20,15 @@ describe('Search.vue', () => {
     // given ...
     // ... and a Vue instance with the component
     Vue.use(Eventbus)
+    Vue.use(Router)
+    const router = new Router({})
     const vm = new Vue({
       components: {
         Search
       },
       template: '<div><search></search></div>',
-      i18n
+      i18n,
+      router
     }).$mount()
     // then ...
     // ... item is undefined
@@ -35,12 +39,15 @@ describe('Search.vue', () => {
     // given ...
     // ... and a Vue instance with the component
     Vue.use(Eventbus)
+    Vue.use(Router)
+    const router = new Router({})
     const vm = new Vue({
       components: {
         Search
       },
       template: '<div><search></search></div>',
-      i18n
+      i18n,
+      router
     }).$mount()
     // ... search box not visible
     expect(vm.$el.querySelector('#search-area > input')).to.be.null
