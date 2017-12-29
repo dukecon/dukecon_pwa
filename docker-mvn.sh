14c82229f5bb9795ddbc20292c0c5cb4849e7f87
Gerd Aschemann <gerd@aschemann.net>
2017-12-29 14:13:15 +0100

    Allow to configure container user home
    and
    - Add MAVEN_CONFIG setting to user home
    - Move build directory to /usr/src
    - Add local .npm volume (for logs)

--- docker-mvn.sh
+++ docker-mvn.sh
@@ -3,9 +3,13 @@
 tty_option=""
 tty -s && tty_option="-t"
 
+: ${USER_HOME:="/root"}
+
 exec docker run -i ${tty_option} --rm \
-  -v "$HOME/.m2:/root/.m2" \
-  -v "$PWD":/usr/src/mymaven \
-  -w /usr/src/mymaven \
+  -v "$HOME/.m2:$USER_HOME/.m2" \
+  -v "$PWD/.npm:$USER_HOME/.npm" \
+  -v "$PWD":/usr/src \
+  -w /usr/src \
+  -e MAVEN_CONFIG=$USER_HOME/.m2 \
   dukecon/dukecon-maven:latest \
   mvn "$@"
