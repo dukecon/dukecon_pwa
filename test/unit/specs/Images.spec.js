6110cb80da6af8f75f02a85aa20f76974b154086
Falk Sippach <sippsack@users.noreply.github.com>
2018-10-01 19:38:53 +0200

    tried to get running this specification, unsuccesfully (#77)
    * tried to get running this specification, unsuccesfully
    
    * fixed test cases by allowing reset for global state of Images.js and handling of inlined images
    
    * fixing test after changing location of favicon.ico

--- test/unit/specs/Images.spec.js
+++ test/unit/specs/Images.spec.js
@@ -4,10 +4,37 @@ import Images from '../../../src/Images'
 describe('Images.js', () => {
   beforeEach(function () {
     moxios.install()
+    Images.reset()
   })
 
   afterEach(function () {
     moxios.uninstall()
+  })
+
+  it('should have default images', done => {
+    // given ....
+    // ... an empty conference image
+    const images = Images.getImages()
+
+    // when ...
+    // ... initialized
+    moxios.wait(function () {
+      let request = moxios.requests.mostRecent()
+      expect(request.url).to.equal('rest/image-resources.json')
+      request.respondWith({
+        status: 200,
+        responseText: '{}'
+      }).then(function () {
+        // then ...
+        // ... image data is filled
+        var image = require('@/assets/img/logo_dukecon.png')
+        var favicon = require('@/assets/img/favicon.ico')
+        expect(images.conferenceImage).to.include(image)
+        expect(images.conferenceFavIcon).to.include(favicon)
+        expect(images.defaultImage).to.include('data:image/gif;base64,R0lGOD')
+        done()
+      })
+    })
   })
 
   it('should load image data asynchronously', done => {
