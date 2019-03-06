51daa83d97e614eba89a594f3d8fe16cdb7bdb23
stephaniep <s.peters@topdesk.com>
2019-03-06 14:27:35 +0100

    #56 add styling (not final but serviceable)

--- src/components/event/LongTalkMarker.vue
+++ src/components/event/LongTalkMarker.vue
@@ -1,8 +1,30 @@
 <template>
   <div class="marker-box alternate" v-if="event.durationInMinutes > thresholdInMinutes">
-    <div class="starts-before" :style="visibilityStart">^</div>
-    <div class="is-long">*</div>
-    <div class="ends-after" :style="visibilityEnd">v</div>
+    <div class="starts-before alternateBack" :style="visibilityStart">
+      <div class="arrow-up">&nbsp;</div>
+    </div>
+    <div class="is-long alternateBack reverse" :class="gradientClass">
+      <svg version="1.1" id="Ebene_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+           width="16px" height="16px" viewBox="0 0 56.69 56.69" enable-background="new 0 0 56.69 56.69" xml:space="preserve">
+        <path opacity="0.3" d="M28.343,0C12.688,0-0.003,12.691-0.003,28.346c0,15.656,12.691,28.347,28.347,28.347
+          c15.655,0,28.346-12.691,28.346-28.347C56.689,12.691,43.998,0,28.343,0L28.343,0z"/>
+                <path d="M28.344,5C41.217,5,51.69,15.473,51.69,28.346c0,12.874-10.474,23.347-23.347,23.347
+          c-12.874,0-23.347-10.474-23.347-23.347C4.997,15.473,15.47,5,28.344,5 M28.344,0C12.688,0-0.003,12.691-0.003,28.346
+          c0,15.656,12.691,28.347,28.347,28.347S56.69,44.002,56.69,28.346C56.69,12.691,43.999,0,28.344,0L28.344,0z"/>
+                <path d="M28.345,30.349c-0.271,0-0.545-0.056-0.809-0.172c-1.01-0.447-1.466-1.628-1.02-2.639L33.4,11.985
+          c0.447-1.01,1.627-1.466,2.639-1.02c1.01,0.447,1.467,1.628,1.02,2.639l-6.884,15.553C29.844,29.904,29.112,30.349,28.345,30.349z"
+                />
+                <path d="M39.686,30.347H28.347c-1.104,0-2-0.896-2-2s0.896-2,2-2h11.339c1.104,0,2,0.896,2,2
+          S40.79,30.347,39.686,30.347z"/>
+                <line fill="none" stroke="#777777" stroke-width="1.5" stroke-linecap="round" stroke-miterlimit="10" x1="28.347" y1="50.033" x2="28.347" y2="52.867"/>
+                <line fill="none" stroke="#777777" stroke-width="1.5" stroke-linecap="round" stroke-miterlimit="10" x1="28.347" y1="3.686" x2="28.347" y2="6.519"/>
+                <line fill="none" stroke="#777777" stroke-width="1.5" stroke-linecap="round" stroke-miterlimit="10" x1="50.104" y1="28.276" x2="52.938" y2="28.276"/>
+                <line fill="none" stroke="#777777" stroke-width="1.5" stroke-linecap="round" stroke-miterlimit="10" x1="3.756" y1="28.276" x2="6.59" y2="28.276"/>
+      </svg>
+    </div>
+    <div class="ends-after alternateBack" :style="visibilityEnd">
+      <div class="arrow-down">&nbsp;</div>
+    </div>
   </div>
 </template>
 <script>
@@ -15,6 +37,15 @@ export default {
     visibilityStart () {
       if (this.event.start === this.event.startOfSlice) {
         return 'visibility: hidden'
+      } else {
+        return ''
+      }
+    },
+    gradientClass () {
+      if (this.event.start === this.event.startOfSlice) {
+        return 'talk-starts' // start gradient
+      } else if (this.event.end <= this.event.endOfSlice) {
+        return 'talk-ends' // end gradient
       } else {
         return ''
       }
@@ -38,12 +69,38 @@ export default {
     display: none;
   }
   .long .marker-box {
-    left: 15px;
+    left: 8px;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+    align-items: stretch;
     position: absolute;
     top: 35px;
     bottom: 10px;
+    .is-long {
+      padding: 2px;
+      flex-grow: 0;
+      &.talk-starts {
+        background-image: linear-gradient(top, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0) 50%, rgba(255, 255, 255, 0));
+        background-repeat: repeat-x;
+      }
+      &.talk-ends {
+        background-image: linear-gradient(top, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0) 30%, rgba(255, 255, 255, 1));
+        background-repeat: repeat-x;
+      }
+    }
+    .ends-after, .starts-before {
+      flex-grow: 1;
+      .arrow-down {
+        height: 100%;
+        background-image: linear-gradient(top, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1));
+        background-repeat: repeat-x;
+      }
+      .arrow-up {
+        height: 100%;
+        background-image: linear-gradient(top, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0));
+        background-repeat: repeat-x;
+      }
+    }
   }
 </style>
