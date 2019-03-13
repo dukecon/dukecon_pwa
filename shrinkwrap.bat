e0eea6993caf8736ce474800d56c93830ba236f5
Alexander Schwartz <alexander.schwartz@gmx.net>
2019-03-13 22:04:16 +0100

    moving to package-lock.json

--- shrinkwrap.bat
+++
@@ -1,7 +0,0 @@
-rem we use this to create a npm-shrinkwrap file
-rem the package-lock.json file includes the reference URLs that might point to an unreachable NPM mirror,
-rem therefore we avoid it.
-set PATH=..\..\..\target\node;%PATH%
-del npm-shrinkwrap.json
-call npm shrinkwrap --dev
-call node filter-shrinkwrap.js
