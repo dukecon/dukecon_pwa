abbf3f8ca48a28c4740fa4db574e00fb64a6070d
Gerd Aschemann <gerd@aschemann.net>
2019-08-11 17:07:52 +0200

    Switch to current karma + puppeteer Node modules
    but also run chrome with hard coded path when on Docker

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -5,14 +5,36 @@
 
 var webpackConfig = require('../../build/webpack.test.conf')
 
+const isDocker = require('is-docker')();
+
+process.env.CHROME_BIN = isDocker ? '/opt/google/chrome/chrome'
+  : function () {
+    const puppeteer = require('puppeteer-core')
+    puppeteer.executablePath()
+  }
+
 module.exports = function (config) {
   config.set({
     // to run in additional browsers:
     // 1. install corresponding karma launcher
-    //    http://karma-runner.github.io/0.13/config/browsers.html
+    //    http://karma-runner.github.io/4.0/config/browsers.html
     // 2. add it to the `browsers` array below.
-    browsers: ['PhantomJS'],
-    frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
+    // logLevel: config.LOG_DEBUG,
+    browsers: ['ChromeHeadlessCustom'],
+    captureConsole: true,
+    customLaunchers: {
+      ChromeHeadlessCustom: {
+        base: 'ChromeHeadless',
+        flags: isDocker ? [
+            '--no-sandbox',
+            '--enable-logging=stderr',
+            '--v=1',
+            '--disable-web-security'
+          ]
+          : []
+      }
+    },
+    frameworks: ['mocha', 'sinon-chai'],
     reporters: ['spec', 'coverage'],
     files: [
       '../../node_modules/babel-polyfill/dist/polyfill.js',
