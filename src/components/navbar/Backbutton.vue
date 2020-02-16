cfcec4d8c062a49dd876f3a74a8adeea2428f122
steffchep <stephp@nexgo.de>
2020-02-16 15:10:11 +0100

    #97 (a11y) make back button work (fix styles)

--- src/components/navbar/Backbutton.vue
+++ src/components/navbar/Backbutton.vue
@@ -1,6 +1,6 @@
 <template>
-  <span style="float: left">
-    <span id="backbutton_before"></span><a id="backbutton" onclick="window.history.back();">{{$t('backbutton')}}</a>
+  <span>
+    <a id="backbutton" href="#" onclick="window.history.back();">{{$t('backbutton')}}</a>
   </span>
 </template>
 
@@ -11,6 +11,32 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
+<style scoped lang="less">
+  span {
+    position: relative;
+    float: left;
+  }
+  a#backbutton {
+    display:none;
+    position: relative;
+    border-radius: 4px;
+    font-size:smaller;
+    font-weight: bold;
+    overflow: hidden;
+    margin: 5px;
+    height: 24px;
+    line-height: 24px;
+    padding: 0 4px 0 4px;
+    border: 1px solid;
+    &:focus {
+      background: #fff;
+    }
+  }
+  @media (max-width: 768px) {
+    a#backbutton {
+      display: inline-block;
+      width: 55px;
+    }
+  }
 
 </style>
