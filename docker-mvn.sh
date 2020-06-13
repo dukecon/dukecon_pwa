5794a33a0ddb13bfe17aaf9e2b6df3aa7ea3d752
Gerd Aschemann <gerd@aschemann.net>
2020-06-13 18:32:37 +0200

    #37 Clean up docker build and fix user home problem
    - The parameters for the Docker build were ordered a little bit
    - The problem of the user home dir was solved by setting a
      system property for the Maven run (due to the `id -u` setting
      of the user id, Java/Maven could not determine the
      user home any more (and used "?") instead

--- docker-mvn.sh
+++ docker-mvn.sh
@@ -5,20 +5,33 @@ set -eu
 tty_option=""
 tty -s && tty_option="-t"
 
-: ${USER_HOME:="/tmp"}
+: ${USER:="dukecon"}
+: ${USER_HOME:="/home"}
 : ${MAVEN_CMD:="mvn -Pheadless"}
+: ${MAVEN_OPTS:=""}
 
-mkdir -p $PWD/.docker/{tmp,node,node_modules,npm}
+mkdir -p ${PWD}/.docker/{home,node,node_modules,npm}
 
 exec docker run -i ${tty_option} --rm \
-  -v "$HOME/.m2:$USER_HOME/.m2" \
-  -v "$PWD/.docker/npm:$USER_HOME/.npm" \
-  -v "$PWD":/usr/src \
-  -v "$PWD/.docker/node:/usr/src/node" \
-  -v "$PWD/.docker/node_modules:/usr/src/node_modules" \
-  -u $(id -u) \
+  -v "${PWD}:/usr/src" \
   -w /usr/src \
-  -e HOME=$USER_HOME \
-  -e MAVEN_CONFIG=$USER_HOME/.m2 \
+  \
+  -u "$(id -u)" \
+  -e "MAVEN_OPTS=-Duser.home=${USER_HOME} ${MAVEN_OPTS}" \
+  \
+  -v "${HOME}/.m2:${USER_HOME}/.m2" \
+  -e "HOME=${USER_HOME}" \
+  -e "USER_HOME_DIR=${USER_HOME}" \
+  -e "MAVEN_CONFIG=${USER_HOME}/.m2" \
+  -e "USER=${USER}" \
+  \
+  -v "${PWD}/.docker/home:${USER_HOME}" \
+  -v "${PWD}/.docker/npm:${USER_HOME}/.npm" \
+  -v "${PWD}/.docker/node:/usr/src/node" \
+  -v "${PWD}/.docker/node_modules:/usr/src/node_modules" \
+  \
   dukecon/dukecon-maven:3.6.3-adoptopenjdk-14 \
   /bin/sh -c "/usr/bin/xvfb-run -s \"-screen 0 2560x1440x8\" -a -e /tmp/xvfb.err -- ${MAVEN_CMD} ${*}"
+
+  maven:3.6.3-adoptopenjdk-14 /bin/sh -c "${MAVEN_CMD} ${*}"
+
