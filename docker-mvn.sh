472aa8fcfc01f7a49580c2b63fc552ba29dbb4b5
Gerd Aschemann <gerd@aschemann.net>
2020-06-13 14:24:20 +0200

    #37 Run Docker build with current UID and use tmp as HOME

--- docker-mvn.sh
+++ docker-mvn.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+set -eu
+
 tty_option=""
 tty -s && tty_option="-t"
 
-: ${USER_HOME:="/root"}
+: ${USER_HOME:="/tmp"}
 : ${MAVEN_CMD:="mvn -Pheadless"}
+
+mkdir -p $PWD/.docker/{tmp,node,node_modules,npm}
 
 exec docker run -i ${tty_option} --rm \
   -v "$HOME/.m2:$USER_HOME/.m2" \
-  -v "$PWD/.npm:$USER_HOME/.npm" \
+  -v "$PWD/.docker/npm:$USER_HOME/.npm" \
   -v "$PWD":/usr/src \
-  -v "$PWD/.docker/node":/usr/src/node \
-  -v "$PWD/.docker/node_modules":/usr/src/node_modules \
+  -v "$PWD/.docker/node:/usr/src/node" \
+  -v "$PWD/.docker/node_modules:/usr/src/node_modules" \
+  -u $(id -u) \
   -w /usr/src \
+  -e HOME=$USER_HOME \
   -e MAVEN_CONFIG=$USER_HOME/.m2 \
   dukecon/dukecon-maven:3.6.3-adoptopenjdk-14 \
-  /bin/sh -c "/usr/bin/xvfb-run -s \"-screen 0 2560x1440x8\" -a -e xvfb.err -- ${MAVEN_CMD} ${*}"
+  /bin/sh -c "/usr/bin/xvfb-run -s \"-screen 0 2560x1440x8\" -a -e /tmp/xvfb.err -- ${MAVEN_CMD} ${*}"
