da12592de20c7ce0565c849034d434b0309e8150
Gerd Aschemann <gerd@aschemann.net>
2020-06-14 10:28:20 +0200

    #37 Persist logs and tmp data (for problem analysis in .docker)

--- docker-mvn.sh
+++ docker-mvn.sh
@@ -10,11 +10,11 @@ tty -s && tty_option="-t"
 : ${MAVEN_CMD:="mvn -Pheadless"}
 : ${MAVEN_OPTS:=""}
 
-mkdir -p ${PWD}/.docker/{home,node,node_modules,npm}
 # Setting of Java user.home property is necessary when using '-u' on Docker call since the Docker container
 # cannot determine a valid Home for this use. Then Maven uses '?' as the user home, settings.xml etc. cannot be
 # found and this results in subsequent problems.
 
+mkdir -p ${PWD}/.docker/{home,logs,node,node_modules,npm,tmp}
 
 exec docker run -i ${tty_option} --rm \
   -v "${PWD}:/usr/src" \
@@ -30,12 +30,14 @@ exec docker run -i ${tty_option} --rm \
   -e "USER=${USER}" \
   \
   -v "${PWD}/.docker/home:${USER_HOME}" \
+  -v "${PWD}/.docker/logs:/usr/src/logs" \
   -v "${PWD}/.docker/npm:${USER_HOME}/.npm" \
   -v "${PWD}/.docker/node:/usr/src/node" \
   -v "${PWD}/.docker/node_modules:/usr/src/node_modules" \
+  -v "${PWD}/.docker/tmp:/tmp" \
   \
   dukecon/dukecon-maven:3.6.3-adoptopenjdk-14 \
-  /bin/sh -c "/usr/bin/xvfb-run -s \"-screen 0 2560x1440x8\" -a -e /tmp/xvfb.err -- ${MAVEN_CMD} ${*}"
+  /bin/sh -c "/usr/bin/xvfb-run -s \"-screen 0 2560x1440x8\" -a -e logs/xvfb.err -- ${MAVEN_CMD} ${*:-}"
 
   maven:3.6.3-adoptopenjdk-14 /bin/sh -c "${MAVEN_CMD} ${*}"
 
