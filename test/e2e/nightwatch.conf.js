1b57bb215d6095ab43fa80748e38bad3da80a41f
Falk Sippach <falk@jug-da.de>
2020-08-12 23:47:15 +0200

    fixed nightwatch e2e test

--- test/e2e/nightwatch.conf.js
+++ test/e2e/nightwatch.conf.js
@@ -34,6 +34,7 @@ module.exports = {
         javascriptEnabled: true,
         acceptSslCerts: true,
         chromeOptions: {
+          w3c: false,
           args: [
             'disable-gpu', 'no-sandbox', 'window-size=1920,1080', 'verbose'
           ]
